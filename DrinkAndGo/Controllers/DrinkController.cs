f8212a2ef132cd8ea5d8d2ec97db636a1b46e22b
Justice Ngwenya <justice@annotations.co.za>
2020-11-19 00:28:31 +0200

    Drink and Go

---
+++ DrinkAndGo/Controllers/DrinkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DrinkAndGo.Data.Interfaces;
+using DrinkAndGo.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkAndGo.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly IDrinkRepository _drinkRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public DrinkController(ICategoryRepository categoryRepository, IDrinkRepository drinkRepository)
+        {
+            _drinkRepository = drinkRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Name = "DotNet, How?";
+            var drinks = _drinkRepository.Drinks;
+            DrinkListViewModel vm = new DrinkListViewModel();
+            vm.Drinks = _drinkRepository.Drinks;
+            vm.CurrentCategory = "DrinkCategory";
+            return View(vm);
+        }
+    }
+}
